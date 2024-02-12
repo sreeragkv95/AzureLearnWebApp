c35916ab49466cfb7c99bcc958666cdb707aa71c
Sreerag K V <Sreerag.K.V2@gds.ey.com>
2024-02-12 14:18:16 +0530

    Add project files.

---
+++ AzureLearnWebApp/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace AzureLearnWebApp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "learnazurekv.database.windows.net";
+        private static string db_user = "sreeragkv95";
+        private static string db_password = "Ragam@123@azure";
+        private static string db_database = "azurelearn";
+
+
+        private void GetConnection()
+        {
+           
+        }
+    }
+}
